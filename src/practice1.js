604489bad6406244c8af5a8dacb13c4be889e38d
liaowenqiang828 <18840965351@163.com>
2020-09-10 22:28:19 +0800

    [Wenqiang Liao] feat: pass practice1 test

--- src/practice1.js
+++ src/practice1.js
@@ -1,2 +1,30 @@
-const parseData
+const parseData = (input) => {
+    const output = [];
+    const columns = [];
+
+    const setDataIntoColumn = (data) => {
+        data.map(item => {
+            let obj = [];
+            item.map((field, index) => {
+                obj.push({name: input.column[index].name, note: field})
+            });
+            columns.push(obj)
+        });
+    }
+
+    const convertColumnIntoObj = columns => {
+        columns.map(column => {
+            let obj = {};
+            column.map(item => {
+                obj[item.name] = item.note
+            });
+            output.push(obj);
+        });
+    }
+
+    setDataIntoColumn(input.data);
+    convertColumnIntoObj(columns);
+    return output;
+}
+
 export { parseData };
