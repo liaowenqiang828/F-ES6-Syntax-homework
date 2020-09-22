fe381e26a9232fd165b2e6d4451833baf4d5c9eb
kevindongzg <dongkevin94@gmail.com>
2020-09-22 13:11:24 +0800

    add TODO

--- src/practice1.js
+++ src/practice1.js
@@ -1,8 +1,13 @@
+// TODO 方法过长，嵌套也过深，可以想想怎么优化
 const parseData = (input) => {
+    // TODO 使用解构的方式取出data与column
+    // TODO 使用map做数组与数组的转化，不用声明新数组
     const output = [];
     const columns = [];
 
+    // TODO 方法可以定义在外面
     const setDataIntoColumn = (data) => {
+        // TODO 不是map的使用场景，查查map的用法
         data.map(item => {
             let obj = [];
             item.map((field, index) => {
--- src/practice2.js
+++ src/practice2.js
@@ -1,4 +1,6 @@
+// TODO 逻辑纰漏，当sections内items顺序不是index顺序时会有问题
 const inject = (items, sections) => {
+    // TODO 不是map的使用场景，查查map的用法
     sections.map((section, ind) => {
         items.splice(section.index + ind, 0, section.content);
     });
